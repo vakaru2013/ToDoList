894fd3a6234d604651b989797e906c1127c70cef
vakaru2013 <18871392@qq.com>
2014-10-03 14:52:43 +0800

    用自定义的类型来代表那个table view controller，然后给add todo item view controller的add和cancel buttons添加了unwind segue，这需要先在destination view controller中新增一个unwind action，unwind segue在运行时判断谁是destination view controller时需要这个action，并且当unwind segue被触发时，这个action会被segue调用。

---
+++ ToDoList/ToDoList/ToDoListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ToDoListTableViewController.h
+//  ToDoList
+//
+//  Created by 宋伟 on 14-10-3.
+//  Copyright (c) 2014年 hellostop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToDoListTableViewController : UITableViewController
+
+@end
